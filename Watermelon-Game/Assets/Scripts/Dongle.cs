cb7397048814b3f8c4f0861bcabc9f0edf508c8a
WooJaeHak <wistm6980@gmail.com>
2024-06-12 21:54:17 +0900

    how to Drag&Drop a Dongle

--- Watermelon-Game/Assets/Scripts/Dongle.cs
+++ Watermelon-Game/Assets/Scripts/Dongle.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (isDrag) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        float rightWall = 4.1f - (transform.localScale.x / 2f);
-        float leftWall = -4.1f + (transform.localScale.x / 2f);
+            float rightWall = 4.1f - (transform.localScale.x / 2f);
+            float leftWall = -4.1f + (transform.localScale.x / 2f);
 
-        if (rightWall < mousePos.x) {
-            mousePos.x = rightWall;
+            if (rightWall < mousePos.x)
+            {
+                mousePos.x = rightWall;
+            }
+            else if (mousePos.x < leftWall)
+            {
+                mousePos.x = leftWall;
+            }
+
+            mousePos.y = 8.0f;
+            mousePos.z = 0f;
+
+            transform.position = mousePos;
         }
-        else if (mousePos.x < leftWall) {
-            mousePos.x = leftWall;
-        }
+    }
 
-        mousePos.y = 8.0f;
-        mousePos.z = 0f;
+    public void Drag() {
+        isDrag = true;
 
-        transform.position = mousePos;
+    }
+    
+    public void Drop() {
+        isDrag = false;
+        rigid.simulated = true; //물리작용 활성
     }
 }
